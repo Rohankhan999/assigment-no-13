511059cc049919a5c9f31108eb5675facded47b2
Rohankhan999 <rohankhan08453@gmail.com>
2024-10-17 19:46:24 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-// // // task 01//
-// // var date = new Date;
-// // console.log(date);
+// task 01//
+// var date = new Date;
+// console.log(date);
 
-// // console.log(date.getFullYear()); 
-// // console.log(date.getHours()); 
-// // console.log(date.getMinutes()); 
-// // console.log(date.getSeconds()); 
-// // console.log(date.getMilliseconds());
-// // console.log(date.getTime());
-
-
-// // var Day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-// // console.log(Day[date.getDay()]);
+// console.log(date.getFullYear()); 
+// console.log(date.getHours()); 
+// console.log(date.getMinutes()); 
+// console.log(date.getSeconds()); 
+// console.log(date.getMilliseconds());
+// console.log(date.getTime());
 
 
-// // var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-// // console.log(months[date.getMonth()]);
+// var Day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+// console.log(Day[date.getDay()]);
 
 
-// // var dates = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-// // console.log(dates[date.getDate()]);
-
-// // // task 02
-
-// // var dateOfBirth = new Date();
-// // dateOfBirth.setFullYear(2007); 
-// // var year = dateOfBirth.getFullYear(); 
-// // console.log(year);
+// var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// console.log(months[date.getMonth()]);
 
 
-// // // task 03
+// var dates = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+// console.log(dates[date.getDate()]);
+
+// task 02
+
+// var dateOfBirth = new Date();
+// dateOfBirth.setFullYear(2007); 
+// var year = dateOfBirth.getFullYear(); 
+// console.log(year);
+
+
+// task 03
 
 // var dateOfBirth = new Date();
 // dateOfBirth.setFullYear(2007,7,4);
@@ -78,3 +78,23 @@
 // } else {
 //     alert("Last 15 days of the month!");
 // }
+
+//  question no 06 //
+
+// let currentDate = new Date(); // Get the current date and time
+// let minutesSinceEpoch = Math.floor(currentDate.getTime() / 60000); // Convert milliseconds to minutes
+
+// document.write(minutesSinceEpoch);
+
+//  question no 07 //
+
+// var timeZone = new Date()
+// var currentHours = timeZone.getHours();
+
+// if(currentHours < 12){
+//     alert("It's AM")}
+//     else{
+//         alert("It's PM")
+//     }
+
+
